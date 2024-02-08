9ce53a0f2523ba3b7ce41ffd0f078d64309c5139
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2024-02-08 12:27:34 +0600

    portfolio autoplay update

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,8 +113,14 @@ var swiper = new Swiper(".portfolio__container", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     mousewheel: true,
     keyboard: true,
